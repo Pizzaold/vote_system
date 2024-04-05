3862ba7e0bae7e54bb648c0ad0415e90d44a4dd0
RaZe <razeslothc@gmail.com>
2024-04-05 14:24:55 +0300

    fix: make the ejs views engine work

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
 const port = 3000;
-
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
