d145aa9081efd9756e99deaf5f030cccec77c5f9
Jürmo Mihhailov <jurmoharak@gmail.com>
2024-04-06 14:12:41 +0300

    Lobby and voting pages

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-	res.render('pages/index');
+	res.render('pages/login');
 });
 
 app.listen(port, () => {
