d54058eb512324fc48d3cad14019eea1508d0b9d
RaZe <razeslothc@gmail.com>
2024-04-09 09:17:03 +0300

    feat(WIP): orm for orm lol

--- src/util/db.js
+++ src/util/db.js
@@ -1,18 +1,41 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
+const hääletus = require('../models/HÄÄLETUS');
 require('dotenv/config');
 
-const sequelize = new Sequelize({
-	dialect: 'mysql',
-	host: process.env.DB_HOST,
-	username: process.env.DB_USER,
-	password: process.env.DB_PASS,
-});
+class DB {
+	constructor() {
+		this.started = false;
+		this.sequelize = new Sequelize({
+			dialect: 'mysql',
+			database: 'Vote_system',
+			host: process.env.DB_HOST,
+			username: process.env.DB_USER,
+			password: process.env.DB_PASS,
+		});
+	}
+	async start() {
+		try {
+			await this.sequelize.authenticate();
+			this.started = true;
+			console.log('Connection has been established successfully.');
+		} catch (error) {
+			console.error('Unable to connect to the database:', error);
+		}
+	}
+	async getUser() {
+		if (!this.started) {
+			throw new Error('Database not started');
+		}
+		const voted = hääletus(this.sequelize, DataTypes);
+		return await voted.findAll();
+	}
+}
 
 (async () => {
-	try {
-		await sequelize.authenticate();
-		console.log('Connection has been established successfully.');
-	} catch (error) {
-		console.error('Unable to connect to the database:', error);
-	}
+	const andmebaas = new DB();
+	await andmebaas.start();
+	const users = await andmebaas.getUser();
+	console.log(users);
 })();
+
+module.exports = DB;
