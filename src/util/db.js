0154a9c41fa9aec184c47f3b5d53a2ad7d7f3065
ThomasTheFknTrainEngine <marten.uiboupin@gmail.com>
2024-04-09 10:23:56 +0300

    FEAT: ORM hook to update LOGI based on user vote

--- src/util/db.js
+++ src/util/db.js
@@ -1,41 +1,66 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const hääletus = require('../models/HÄÄLETUS');
+const Hääletus = require('../models/HÄÄLETUS');
+const Logi = require('../models/LOGI');
 require('dotenv/config');
 
 class DB {
-	constructor() {
-		this.started = false;
-		this.sequelize = new Sequelize({
-			dialect: 'mysql',
-			database: 'Vote_system',
-			host: process.env.DB_HOST,
-			username: process.env.DB_USER,
-			password: process.env.DB_PASS,
-		});
-	}
-	async start() {
-		try {
-			await this.sequelize.authenticate();
-			this.started = true;
-			console.log('Connection has been established successfully.');
-		} catch (error) {
-			console.error('Unable to connect to the database:', error);
-		}
-	}
-	async getUser() {
-		if (!this.started) {
-			throw new Error('Database not started');
-		}
-		const voted = hääletus(this.sequelize, DataTypes);
-		return await voted.findAll();
-	}
+    constructor() {
+        this.started = false;
+        this.sequelize = new Sequelize({
+            dialect: 'mysql',
+            database: 'Vote_system',
+            host: process.env.DB_HOST,
+            username: process.env.DB_USER,
+            password: process.env.DB_PASS,
+        });
+    }
+    async start() {
+        try {
+            await this.sequelize.authenticate();
+            this.started = true;
+            console.log('Connection has been established successfully.');
+        } catch (error) {
+            console.error('Unable to connect to the database:', error);
+        }
+    }
+    async initializeModels() {
+        if (!this.started) {
+            throw new Error('Database not started');
+        }
+        this.HääletusModel = Hääletus(this.sequelize, DataTypes);
+        this.LogiModel = Logi(this.sequelize, DataTypes);
+
+        this.HääletusModel.addHook('afterUpdate', 'logUpdate', async (instance) => {
+            const { kasutaja_id, otsus } = instance._previousDataValues;
+            const tegevus_aeg = new Date();
+            await this.LogiModel.create({ kasutaja_id, otsus, tegevus_aeg });
+        });
+    }
+    async getUser() {
+        if (!this.started) {
+            throw new Error('Database not started');
+        }
+        return await this.HääletusModel.findAll();
+    }
+    async logAction(kasutaja_id, otsus) {
+        if (!this.started) {
+            throw new Error('Database not started');
+        }
+        const tegevus_aeg = new Date();
+        await this.LogiModel.create({ kasutaja_id, otsus, tegevus_aeg });
+        await this.HääletusModel.update({ otsus }, { where: { kasutaja_id } });
+		return await this.LogiModel.findAll();
+    }
 }
 
 (async () => {
-	const andmebaas = new DB();
-	await andmebaas.start();
-	const users = await andmebaas.getUser();
-	console.log(users);
+    const database = new DB();
+    await database.start();
+    await database.initializeModels();
+    const log = await database.logAction(3, 'poolt');
+    const users = await database.getUser();
+    console.log(log);
+    console.log(users);
 })();
 
 module.exports = DB;
