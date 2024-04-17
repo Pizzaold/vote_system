f50013f54011e0c835b5d4df1809df995b333ccd
RaZe <razeslothc@gmail.com>
2024-04-17 08:48:34 +0300

    enhancement: loader for the lobby

--- src/views/js/countdown.js
+++ src/views/js/countdown.js
@@ -1,4 +1,5 @@
 const countdown = document.getElementById('countdown');
+const loader = document.getElementById('loader');
 const voted = countdown.getAttribute('voted');
 const endTime = new Date(countdown.getAttribute('data-time')).getTime() + new Date().getTimezoneOffset() * 60000;
 
@@ -6,11 +7,13 @@ const timer = setInterval(() => {
     const now = new Date().getTime();
     const distance = endTime - now;
     if (isNaN(distance)){
+        loader.style.display = 'none';
         countdown.innerHTML = 'Voting has not started';
     } else if (distance <= 0) {
         if (voted === 'false') {
             window.location.href = '/voting';
         } else if (voted === 'true') {
+            loader.style.display = 'none';
            countdown.innerHTML = 'Voting has ended';
         }
         clearInterval(timer);
